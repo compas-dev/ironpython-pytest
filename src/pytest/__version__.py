a6c8ad663fc66e8c72d8245874e9234189c88cd3
Gonzalo Casas <casas@arch.ethz.ch>
2021-02-13 09:26:20 +0100

    Bump version to 0.4.0

--- src/pytest/__version__.py
+++ src/pytest/__version__.py
@@ -6,6 +6,6 @@ __copyright__ = 'Copyright 2020 Gramazio Kohler Research'
 __license__ = 'MIT'
 __email__ = 'gramaziokohler@arch.ethz.ch'
 
-__version__ = '0.3.3'
+__version__ = '0.4.0'
 
 __all__ = ['__author__', '__email__', '__copyright__', '__license__', '__version__']
